fd7e2d33b67f684fd67978cf4c00584e78cb0d41
Adam Basiak <basiakus@gmail.com>
2017-12-17 11:26:01 +0100

    push

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ console.log(`imię: ${name}; nazwisko: ${surName}`);
 // zadanie 2
 const multiply = (a, b = 1) => a * b;
 //zadanie 3
-const average = (...args) => [...args].reduce((a, b) => a + b, 0) / args.length;
+const average = (...args) => args.reduce((a, b) => a + b, 0) / args.length;
 //zadanie 4
 const notes = [1, 5, 5, 5, 4, 3, 3, 2, 1]
 average(...notes);
@@ -14,7 +14,7 @@ average(...notes);
 const person = [1, 4, 'Iwona', false, 'Nowak'];
 //const [year , month, firstName, isMarried, lastName] = person;
 const [, ,firstName, ,lastName] = person
-console.log([, ,firstName, ,lastName]);
+console.log(firstName, lastName);
 
 
 
